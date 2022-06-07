8053fc2c76b90fdf38b26a937b6efb2537ed37cf
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-06-07 09:52:15 +0100

    added my first framer animation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,7 @@ import './app.scss';
 
 const App = () => {
     console.log(motion)
+
     return (
         <motion.div animate={{top:'10%', fontSize:'100px', transition:{type:'spring', bounce:1, duration:10}}} className='AppMain'>
             Am talking to you hellow
